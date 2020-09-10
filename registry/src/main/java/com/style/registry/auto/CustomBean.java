0e1464436a6aa7567e6b1f9fece89fb76045092b
leon <1311552105@qq.com>
2020-09-10 16:45:09 +0800

    update example

--- registry/src/main/java/com/style/registry/auto/CustomBean.java
+++ registry/src/main/java/com/style/registry/auto/CustomBean.java
@@ -1,34 +1,19 @@
 package com.style.registry.auto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author leon
  * @date 2020-07-13 16:51:25
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomBean {
+
 	private Integer id;
+
 	private String name;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public CustomBean(Integer id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-
-	public CustomBean() {
-	}
 }
